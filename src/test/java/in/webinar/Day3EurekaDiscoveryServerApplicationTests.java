33bdb70c4ef80ba8dcd45fbd41ddfa72fbf8ec85
Santosh Mondal <santosh.ece06@gmail.com>
2021-03-03 14:20:24 +0530

    discover server.

---
+++ src/test/java/in/webinar/Day3EurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.webinar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3EurekaDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
